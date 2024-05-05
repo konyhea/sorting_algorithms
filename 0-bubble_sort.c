fb1c46e12b62a408963b82485e55cca58551040b
Nduka Konyhea Prince <ndukaprincek@gmail.com>
2024-05-04 18:56:00 -0700

    f

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,34 @@
 #include "sort.h"
 
 /**
- * bubble_sort - the bubble sort algo
+ * bubble_sort - bubble sort algorithm
  * @array: the array to sort
- * @size: the number of data in the array
+ * @size: the number of data to sort
  */
 
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j;
-        int temp, swapped;
+	size_t i, j;
+	int temp, swap;
 
-        if (!array || !size)
-                return;
+	if (!array || !size)
+		return;
 
-        for (i = 0; i < size - 1; i++)
-        {
-                swapped = 0;
-                for (j = 0; j < size - 1 - i; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
-                                swapped = 1;
-                                print_array(array, size);
-                        }
-                }
-                if (!swapped)
-                        break;
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		swap = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+		if (!swap)
+			break;
+	}
 }
